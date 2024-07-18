713e51e0b3cd43fec29e76f22204806808ff979a
vinhdk <49849883+vinhdk@users.noreply.github.com>
2024-07-18 09:38:48 +0000

    [vinhdk] refactor(routing): added search routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AdminstrationModule } from './pages/adminstration/adminstration.module'
 import { DashboardModule } from './pages/dashboard/dashboard.module';
 import { DesktopModule } from './pages/desktop/desktop.module';
 import { RootNodeModule } from './pages/root-node/root-node.module';
+import { SearchModule } from './pages/search/search.module';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     AdminstrationModule,
     DashboardModule,
     DesktopModule,
-    RootNodeModule
+    RootNodeModule,
+    SearchModule
   ],
   exports: [RouterModule]
 })
